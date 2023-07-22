5c06940a4d34c6c24b415201dca6ec6ed50d6be1
Ali <net_lina95@mail.ru>
2023-07-22 13:03:32 +0300

    1-2 done

---
+++ 02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+void InputArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101);
+
+}
+
+int ReleaseArray(int[] array)
+{
+    int sum = 0;
+     for (int i = 0; i < array.Length; i++)
+        if (i % 2  == 0)
+            sum = sum + array[i];
+    return sum;
+}
+
+
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
---
+++ 03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+void InputArray(double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 101);
+
+}
+
+double ReleaseArray(double[] array)
+
+
+
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [] array = new double[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
